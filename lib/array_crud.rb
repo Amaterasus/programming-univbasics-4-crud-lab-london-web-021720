d704ae8c04470e54cbb2f797d9a211ef68d672e6
Skye Geraghty <skyetay03@googlemail.com>
2020-01-24 05:41:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["something", "else"]
+  array = ["something", "else", "requires", "four"]
 end
 
 def add_element_to_end_of_array(array, element)
